ec61314fc847ed57800705d996069867ea9694cc
SIX <83570379+hung319@users.noreply.github.com>
2024-09-08 21:40:46 +0700

    Update root-curl.sh

--- root-curl.sh
+++ root-curl.sh
@@ -25,13 +25,13 @@ if [ ! -e $ROOTFS_DIR/.installed ]; then
   echo "#"
   echo "#######################################################################################"
 
-  read -p "Do you want to install Ubuntu? (y/n): " install_ubuntu
+  read -p "Do you want to install Ubuntu 22.04? (y/n): " install_ubuntu
 fi
 
 case $install_ubuntu in
   [y])
     curl -L --retry $max_retries --connect-timeout $timeout -o /tmp/rootfs.tar.gz \
-      "http://cdimage.ubuntu.com/ubuntu-base/releases/20.04/release/ubuntu-base-20.04.4-base-${ARCH_ALT}.tar.gz"
+      "http://cdimage.ubuntu.com/ubuntu-base/releases/22.04/release/ubuntu-base-22.04-base-${ARCH_ALT}.tar.gz"
     tar -xf /tmp/rootfs.tar.gz -C $ROOTFS_DIR
     ;;
   *)
