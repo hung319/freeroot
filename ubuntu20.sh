176a0c01ac3b1da9f84a0e81599067906770219f
SIX <83570379+hung319@users.noreply.github.com>
2024-09-10 19:35:48 +0700

    Update ubuntu20.sh

--- ubuntu20.sh
+++ ubuntu20.sh
@@ -3,7 +3,7 @@
 ROOTFS_DIR=$(pwd)
 export PATH=$PATH:~/.local/usr/bin
 max_retries=50
-timeout=10
+timeout=1
 ARCH=$(uname -m)
 PROOT_VERSION="5.3.0"
 
@@ -26,21 +26,12 @@ if [ ! -e $ROOTFS_DIR/.installed ]; then
   echo "#"
   echo "#######################################################################################"
 
-  read -p "Do you want to install Ubuntu? (y/n): " install_ubuntu
-fi
+  echo "Installing Ubuntu 20.04..."
 
-case $install_ubuntu in
-  [y])
-    curl -L --retry $max_retries --connect-timeout $timeout -o /tmp/rootfs.tar.gz \
-      "http://cdimage.ubuntu.com/ubuntu-base/releases/20.04/release/ubuntu-base-20.04.4-base-${ARCH_ALT}.tar.gz"
-    tar -xf /tmp/rootfs.tar.gz -C $ROOTFS_DIR
-    ;;
-  *)
-    echo "Skipping Ubuntu installation."
-    ;;
-esac
+  curl -L --retry $max_retries --connect-timeout $timeout -o /tmp/rootfs.tar.gz \
+    "http://cdimage.ubuntu.com/ubuntu-base/releases/20.04/release/ubuntu-base-20.04.4-base-${ARCH_ALT}.tar.gz"
+  tar -xf /tmp/rootfs.tar.gz -C $ROOTFS_DIR
 
-if [ ! -e $ROOTFS_DIR/.installed ]; then
   mkdir $ROOTFS_DIR/usr/local/bin -p
   curl -L --retry $max_retries --connect-timeout $timeout -o $ROOTFS_DIR/usr/local/bin/proot "https://github.com/proot-me/proot/releases/download/v${PROOT_VERSION}/proot-v${PROOT_VERSION}-${ARCH}-static"
 
@@ -58,9 +49,7 @@ if [ ! -e $ROOTFS_DIR/.installed ]; then
   done
 
   chmod 755 $ROOTFS_DIR/usr/local/bin/proot
-fi
 
-if [ ! -e $ROOTFS_DIR/.installed ]; then
   printf "nameserver 1.1.1.1\nnameserver 1.0.0.1" > ${ROOTFS_DIR}/etc/resolv.conf
   rm -rf /tmp/rootfs.tar.xz /tmp/sbin
   touch $ROOTFS_DIR/.installed
